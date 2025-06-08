885590e1725cf23a9f3ae11a65f4662b966150f4
vanoke <jel.iv4n@mail.ru>
2025-06-08 15:50:23 +0500

    3 workshop correct
    прошлый коммит некорректно отправил, из-за чего новые файлы с выполненной работой не загрузились

---
+++ Assets/Scripts/RagdollControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollControl : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody[] allRigidbodies;
+
+    private void Awake()
+    {
+        for (int i = 0; i < allRigidbodies.Length; i++)
+        {
+            allRigidbodies[i].isKinematic = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MakePhysics();
+        }
+    }
+
+    public void MakePhysics()
+    {
+        animator.enabled = false;
+        for (int i = 0; i < allRigidbodies.Length; i++)
+        {
+            allRigidbodies[i].isKinematic = false;
+        }
+
+
+    }
+
+}
